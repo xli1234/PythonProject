6ea7c974d112c039d4f37707208c0c108f98fb57
xli1234 <55726294+xli1234@users.noreply.github.com>
2019-10-02 09:55:44 -0700

    Link directory for restaurants and apartments in main

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import sys
 from pathlib import Path
 crime_path = str(Path(__file__).parent.absolute()) + '/crime'
 imap_path = str(Path(__file__).parent.absolute()) + '/imap'
+rest_path = str(Path(__file__).parent.absolute()) + '/restaurant'
+apt_path = str(Path(__file__).parent.absolute()) + '/apartments'
 sys.path.insert(1, crime_path)
 sys.path.insert(1, imap_path)
+sys.path.insert(1, rest_path)
+sys.path.insert(1, apt_path)
 
 # import individual modules
 import crime_final as crime
@@ -14,4 +18,4 @@ import imap
 
 # Example of using data from module
 print(crime.df2)
-print(imap.map_cache)
+print(imap.map_data)
