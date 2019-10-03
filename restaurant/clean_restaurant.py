58fb610bfebb188b35c381e4c46cab5739229466
shenshopping <34395567+shenshopping@users.noreply.github.com>
2019-10-03 00:25:31 -0400

    add restaurant related

---
+++ restaurant/clean_restaurant.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+
+
+file_path = 'business.json' # downloaded from https://www.yelp.com/dataset
+f = open('business.json', 'r')
+i = 0
+cols = ['business_id', 'name', 'address', 'city', 'state', 'postal_code', 'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'attributes', 'categories', 'hours']
+df = pd.DataFrame(columns = cols)
+df['attributes'] = df['attributes'].astype(str)
+for line in f.readlines():
+    if '"city":"Pittsburgh"' in line:
+#         print(eval(line.strip().replace('null', '""')))
+        df = pd.concat([df, pd.DataFrame(eval(line.strip().replace('null', '""')), index=[i])], ignore_index=True)
+        i = i + 1
+f.close()
+
+
+# only columns we will need
+cols = ['name', 'address', 'city', 'state', 'postal_code', 'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'categories']
+# only zip code in (15213, 15217, 15232)
+# save to csv format
+df[((df.postal_code == '15213') | (df.postal_code == '15217') | (df.postal_code == '15232'))][cols].to_csv('restaurant.csv', index = False) 
+
+
+
+
+
+
+
