b2301ab71e503cc28f0af51ee477b6511ccfa6f9
shenshopping <34395567+shenshopping@users.noreply.github.com>
2019-10-03 18:15:44 -0400

    add restaurant&visualization part

--- main.py
+++ main.py
@@ -12,10 +12,30 @@ sys.path.insert(1, imap_path)
 sys.path.insert(1, rest_path)
 sys.path.insert(1, apt_path)
 
+
 # import individual modules
 import crime_final as crime
 import imap
+from restaurant.clean_restaurant import clean_restaurant
+from visualization.map_visualization import map_visualize
+
 
 # Example of using data from module
 print(crime.df2)
 print(imap.map_data)
+
+
+# run clean_restaurant()
+clean_restaurant()
+print('restaurant data clean: done')
+
+
+# call map_visualize
+apt_name = 'Royal York'
+apt_lat = 40.453150
+apt_lng = -79.953920
+crime_percentage = 0.398
+distance = 1385
+time = [366, 1006, 371]
+map_visualize(apt_name=apt_name, apt_lat=apt_lat, apt_lng=apt_lng, 
+				crime=crime_percentage, distance=distance, time=time)
