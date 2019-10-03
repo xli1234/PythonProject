4e081cbb6089546d4d93b52b786bf2d6b628f091
xli1234 <55726294+xli1234@users.noreply.github.com>
2019-10-03 02:27:48 -0700

    update cached data and map code

--- imap/imap.py
+++ imap/imap.py
@@ -23,7 +23,7 @@ try:
     map_data = map_data.set_index('APT')
 except:
     # If no previous valid cache, let's begin with an empty DF
-    map_data = pd.DataFrame(columns=['DIS_DRI', 'TIM_DRI', ' DIS_WAL', 'TIM_WAL', 'DIS_BIC', 'TIM_BIC'])
+    map_data = pd.DataFrame(columns=['DRIV_DIST', 'DRIV_TIME', ' WALK_DIST', 'WALK_TIME', 'BIKE_DIST', 'BIKE_TIME'])
     map_data.index.name = 'APT'
     
 # import or use apt+address from apartment module
@@ -31,14 +31,17 @@ apt_list = [
     'Royal York',
     'Oakland Apartments',
     'Oak Hill Apartments',
+    'Wendover Apartments',
 ]
 addr_list = [ # replace with zip_list if needed
     '3955 Bigelow Blvd, Pittsburgh, PA 15213',
     '4629 Bayard St, Pittsburgh, PA 15213',
-    '3955 Bigelow Blvd, Pittsburgh, PA 15213',
+    '475 Garner Ct Suite 215, Pittsburgh, PA 15213'
+    '5562 Hobart St, Pittsburgh, PA 15217',
 ]
 
 # Map Data Scraping Function
+#pat = r'\[[0-9]*,\\"[0-9]* (?:ft|miles?)\\",[0-9]*\]\\n,\[[0-9]*,\\"[0-9]* min\\"\]'
 def shortest_distance_time(arg):
     text = str(arg) # arg = bsyc
     end_idx = 0
@@ -65,17 +68,24 @@ def shortest_distance_time(arg):
         end_idx = text.find(',', start_idx)
         # small bug, we are using new time not shortest_time if same shortest_distance
         shortest_time = int(text[start_idx : end_idx])
+    if shortest_distance == 0: # small bug for ft distance, tmp work around
+        return ['< 160', 'FAST']
     return [shortest_distance, shortest_time]
 
 # Let's not support transit now, noise in sub-routes
 travel_mode = ['driving', 'walking', 'bicycling'] #, 'transit']
 
 # Function to create query, open url, call scraping and return one data entry
-# One data entry contains value for ['DIS_DRI', 'TIM_DRI', ' DIS_WAL', 'TIM_WAL', 'DIS_BIC', 'TIM_BIC']
 def goog_map(apt, addr):
     goog_map_data = []
     for tm in travel_mode:
-        link = 'https://www.google.com/maps/dir/?api=1&origin='+(apt+' '+addr).replace(' ','+')+'&destination=CMU&travelmode='+tm
+        destination = 'Carnegie Mellon University, 5000 Forbes Ave, Pittsburgh, PA 15213'.replace(' ','+')
+        idx = apt.find('#')
+        if (idx > 0):
+            origin = (apt[:idx] + addr).replace(' ','+')
+        else:
+            origin = (apt + ' ' + addr).replace(' ','+')
+        link = 'https://www.google.com/maps/dir/?api=1&origin='+origin+'&destination='+destination+'&travelmode='+tm
         html = urlopen(link)
         bsyc = BeautifulSoup(html.read(), "lxml")
         time.sleep(5)
@@ -86,20 +96,26 @@ def goog_map(apt, addr):
         goog_map_data += shortest_distance_time(bsyc)
     return goog_map_data
 
-
 # Search
+iprint('Current map_data:')
+iprint(map_data)
+idx = 0
 for apt,addr in zip(apt_list, addr_list):
+    idx += 1;
     # Skip apt that exists in DF
-    if apt in map_data.index:
+    if apt == 'Address Not Disclosed' or apt in map_data.index:
         continue
     try:
         # Add row to DataFrame
+        iprint('Updating...(', idx,'/',len(apt_list),')')
         map_data.loc[apt] = goog_map(apt, addr)
+        iprint(map_data.loc[apt].to_frame().T)
     except:
         iprint('Unable to read more map. Save current data.')
         break
 
 # Check updated DF
+iprint('\nUpdating map_data done.')
 iprint(map_data)
 # Save our DF
 map_data.to_csv(filename)
