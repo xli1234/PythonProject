1b491d137adbf7a9ab08c2ad439b2f46139fe766
animish16 <55815259+animish16@users.noreply.github.com>
2019-10-04 16:17:22 -0400

    Update house_listings.py
    Moved the scraping code inside a function.
    Call scrape_house_data() function to scrape houses and create a csv.

--- apartment/house_listings.py
+++ apartment/house_listings.py
@@ -1,14 +1,8 @@
-# Enter desired zip codes below
-zipcodes = ['15213', '15217', '15232'] # Required zip codes (USA only)
-
 # Initializations
 import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 from pathlib import Path
-
-# CSV file path and name
-filename = str(Path(__file__).parent.absolute())+'/house_cache.csv'
 
 # Some essential functions
 def house_attr_list(soup, tag, css_identifier, find_children = False, child = 0, content_num = 0):
@@ -40,81 +34,105 @@ def url_to_soup(url, headers = {'User-Agent': 'Mozilla/5.0'}, markup = 'lxml'):
     Returns a BeautifulSoup object for the provided URL.
     Inputs:
         url: HTTP or HTTPS URL without a redirect
-        headers: Pretend to be a browsing agent (default = Mozilla 5.0)
+        headers: User agent to pretent to be (default = Mozilla 5.0 agent)
         markup: BeautifulSoup markup (default = lxml)
     """
     # Return a BS object for the URL and the agent provided
     return BeautifulSoup(urlopen(Request(url, headers = headers)).read(), markup)
 
-# Blank Pandas DataFrame to hold house data
-house_data = pd.DataFrame(columns = ['Zip', 'Street', 'Region', 'Price', 'Bedrooms', 'Bathrooms',
-                                     'Floorspace', 'Pet_friendly', 'Furnished'])
 
-# Traverse each zip code
-for z in zipcodes:
-    # Traverse the listing page for the zipcode on Trulia.com
-    soup = url_to_soup('https://www.trulia.com/for_rent/'+str(z)+'_zip/')
+def scrape_house_data(
+        zipcodes = ['15213', '15217', '15232'],
+        verbose = True,
+        filename = str(Path(__file__).parent.absolute())+'/house_cache.csv'
+        ):
+    """
+    Scrapes data from Trulia.com for each provided zipcode and creates apartment data cache.
+    Inputs:
+        zipcodes: A Python tuple or list of US zipcodes (default = ['15213', '15217', '15232'])
+        verbose: Whether to print scraping status or not (default = True)
+        filename: The name of the file with an extentsion and a complete path \
+                    \ (default = 'house_cache.csv' inside the same directory as this py file)
+    """
+    # Blank Pandas DataFrame to hold house data
+    house_data = pd.DataFrame(columns = ['Zip', 'Street', 'Region', 'Price', 'Bedrooms', 'Bathrooms',
+                                         'Floorspace', 'Pet_friendly', 'Furnished'])
     
-    # Find how many pages are there in the listing
-    num_pages = house_attr_list(soup, 'li', {'data-testid': 'pagination-page-link'}, True, 1)[-1]
-    num_pages = int(num_pages)
+    # Print a beginning message
+    if verbose: print('Scraping house listings from Trulia');
     
-    # Traverse each page in the listing for the zipcode
-    for i in range(1, num_pages+1):
-        # Show URL of the current page
-        print('Traversing page: https://www.trulia.com/for_rent/'+str(z)+'_zip/'+str(i)+'_p/')
+    # Traverse each zip code
+    for z in zipcodes:
+        # Print current zipcode
+        if verbose: print('\nFetching listings for zip: ' + str(z));
         
-        # Create a BeautifulSoup object
-        soup = url_to_soup('https://www.trulia.com/for_rent/'+str(z)+'_zip/'+str(i)+'_p/')
+        # Traverse the listing page for the zipcode on Trulia.com
+        soup = url_to_soup('https://www.trulia.com/for_rent/'+str(z)+'_zip/')
         
-        # Find all the houses listed on the current page
-        houses = soup('div', {'data-testid': 'home-card-rent'})
-        # Print how many houses are on the current page
-        print("Found " + str(len(houses)) + " houses")
+        # Find how many pages are there in the listing
+        num_pages = house_attr_list(soup, 'li', {'data-testid': 'pagination-page-link'}, True, 1)[-1]
+        num_pages = int(num_pages)
         
-        # Traverse each house and fetch its details
-        for h in houses:
-            # Blank dictionary to be updated and appended to the main DataFrame
-            house_info = {}
+        # Traverse each page in the listing for the zipcode
+        for i in range(1, num_pages+1):
             
-            # Zip
-            house_info['Zip'] = z
-            # Street
-            house_info['Street'] = [h('div', {'data-testid': 'property-street'})[0].contents[0]]
-            # Region
-            house_info['Region'] = [h('div', {'data-testid': 'property-region'})[0].contents[0]]
-            # Price
-            house_info['Price'] = [h('div', {'data-testid': 'property-price'})[0].contents[0].replace('/mo', '').replace('$', '').replace(',', '')]
-            # Bedrooms
-            try:
-                house_info['Bedrooms'] = [h('div', {'data-testid': 'property-beds'})[0].contents[0].replace('bd', '')]
-            except IndexError:
-                house_info['Bedrooms'] = ['']
-            # Bathrooms
-            try:
-                house_info['Bathrooms'] = [h('div', {'data-testid': 'property-baths'})[0].contents[0].replace('ba', '')]
-            except IndexError:
-                house_info['Bathrooms'] = ['']
-            # Floorspace
-            try:
-                house_info['Floorspace'] = [h('div', {'data-testid': 'property-floorSpace'})[0].contents[0].replace('sqft', '').replace(',', '')]
-            except IndexError:
-                house_info['Floorspace'] = ['']
-            # If pet friendly
-            pf = h('div', {'data-testid': 'property-tags'})[0]
-            if str(pf).find('PET FRIENDLY') != -1:
-                house_info['Pet_friendly'] = ['Y']
-            else:
-                house_info['Pet_friendly'] = ['N']
-            # If furnished
-            fr = h('div', {'data-testid': 'property-tags'})[0]
-            if str(fr).find('FURNISHED') != -1:
-                house_info['Furnished'] = ['Y']
-            else:
-                house_info['Furnished'] = ['N']
+            # Create a BeautifulSoup object
+            soup = url_to_soup('https://www.trulia.com/for_rent/'+str(z)+'_zip/'+str(i)+'_p/')
             
-            # Form a DataFrame to be appended
-            house_data = house_data.append(pd.DataFrame(house_info))
+            # Find all the houses listed on the current page
+            houses = soup('div', {'data-testid': 'home-card-rent'})
+            # Print how many houses are on the current page
+            if verbose: print('Traversing: https://www.trulia.com/for_rent/'+str(z)+'_zip/'+str(i)+'_p/ (' + str(len(houses)) + ' houses)')
+            
+            # Traverse each house and fetch its details
+            for h in houses:
+                # Blank dictionary to be updated and appended to the main DataFrame
+                house_info = {}
+                
+                # Zip
+                house_info['Zip'] = z
+                # Street
+                house_info['Street'] = [h('div', {'data-testid': 'property-street'})[0].contents[0]]
+                # Region
+                house_info['Region'] = [h('div', {'data-testid': 'property-region'})[0].contents[0]]
+                # Price
+                house_info['Price'] = [h('div', {'data-testid': 'property-price'})[0].contents[0].replace('/mo', '').replace('$', '').replace(',', '')]
+                # Bedrooms
+                try:
+                    house_info['Bedrooms'] = [h('div', {'data-testid': 'property-beds'})[0].contents[0].replace('bd', '')]
+                except IndexError:
+                    house_info['Bedrooms'] = ['']
+                # Bathrooms
+                try:
+                    house_info['Bathrooms'] = [h('div', {'data-testid': 'property-baths'})[0].contents[0].replace('ba', '')]
+                except IndexError:
+                    house_info['Bathrooms'] = ['']
+                # Floorspace
+                try:
+                    house_info['Floorspace'] = [h('div', {'data-testid': 'property-floorSpace'})[0].contents[0].replace('sqft', '').replace(',', '')]
+                except IndexError:
+                    house_info['Floorspace'] = ['']
+                # If pet friendly
+                pf = h('div', {'data-testid': 'property-tags'})[0]
+                if str(pf).find('PET FRIENDLY') != -1:
+                    house_info['Pet_friendly'] = ['Y']
+                else:
+                    house_info['Pet_friendly'] = ['N']
+                # If furnished
+                fr = h('div', {'data-testid': 'property-tags'})[0]
+                if str(fr).find('FURNISHED') != -1:
+                    house_info['Furnished'] = ['Y']
+                else:
+                    house_info['Furnished'] = ['N']
+                
+                # Form a DataFrame to be appended
+                house_data = house_data.append(pd.DataFrame(house_info))
+                
+        # Print zip done message
+        if verbose: print('Zip ' + z + ' done')
+        
+    # Export data to a CSV file in the current folder
+    house_data.to_csv(filename, index = False)
     
-# Export data to a CSV file in the current folder
-house_data.to_csv(filename, index = False)
+    # Print success message
+    if verbose: print('\nSuccess! Apartment data cache has been updated.')
