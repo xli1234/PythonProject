9b2d3993a8dd17b181e83978805e9f5c0ea69944
xli1234 <55726294+xli1234@users.noreply.github.com>
2019-10-04 01:31:36 -0700

    adjust imap for apartment module, cached all new data

--- imap/imap.py
+++ imap/imap.py
@@ -16,30 +16,40 @@ def iprint(*args):
             print(arg, end=' ')
         print('')
 
-# Load
-filename = str(Path(__file__).parent.absolute())+'/map_cache.csv'
+# Files
+mapFile = str(Path(__file__).parent.absolute())+'/map_cache.csv'
+houseFile = str(Path(__file__).parent.absolute())+'/../apartment/house_cache.csv'
+
+# Load map_data DF
 try:
     # Get DataFrame from cache if any
-    map_data = pd.read_csv(filename)
+    map_data = pd.read_csv(mapFile)
     map_data = map_data.set_index('APT')
 except:
     # If no previous valid cache, let's begin with an empty DF
     map_data = pd.DataFrame(columns=['DRIV_DIST', 'DRIV_TIME', ' WALK_DIST', 'WALK_TIME', 'BIKE_DIST', 'BIKE_TIME'])
     map_data.index.name = 'APT'
     
-# import or use apt+address from apartment module
-apt_list = [
-    'Royal York',
-    'Oakland Apartments',
-    'Oak Hill Apartments',
-    'Wendover Apartments',
-]
-addr_list = [ # replace with zip_list if needed
-    '3955 Bigelow Blvd, Pittsburgh, PA 15213',
-    '4629 Bayard St, Pittsburgh, PA 15213',
-    '475 Garner Ct Suite 215, Pittsburgh, PA 15213'
-    '5562 Hobart St, Pittsburgh, PA 15217',
-]
+# Load house_data DF exported from apartment module
+try:
+    house_data = pd.read_csv(houseFile)
+    apt_list = house_data['Street'].values
+    zip_list = house_data['Zip'].values
+    addr_list = ['Pittsburgh, PA '+str(zip_list[i]) for i in range(len(zip_list))]
+except:
+    # Hardcode if no export from apartment module
+    apt_list = [
+        'Royal York',
+        'Oakland Apartments',
+        'Oak Hill Apartments',
+        'Wendover Apartments',
+    ]
+    addr_list = [ # replace with zip_list if needed
+        '3955 Bigelow Blvd, Pittsburgh, PA 15213',
+        '4629 Bayard St, Pittsburgh, PA 15213',
+        '475 Garner Ct Suite 215, Pittsburgh, PA 15213'
+        '5562 Hobart St, Pittsburgh, PA 15217',
+    ]
 
 # Map Data Scraping Function
 # Sample format [1987,\"1.2 miles\",1]\n,[366,\"6 min\"]
@@ -54,9 +64,6 @@ def shortest_distance_time(arg):
         if end_idx == -1:
             break
         start_idx = text.rfind('[', 0, end_idx)+1
-        #start_idx = end_idx
-        #while(text[start_idx-1] != '['):
-        #    start_idx -= 1
         end_idx = text.find(',', start_idx)
         distance = int(text[start_idx : end_idx])
         # Only do shortest distance
@@ -82,12 +89,19 @@ def goog_map(apt, addr):
     goog_map_data = []
     for tm in travel_mode:
         destination = 'Carnegie Mellon University, 5000 Forbes Ave, Pittsburgh, PA 15213'.replace(' ','+')
+        # Remove unit number
         idx = apt.find('#')
         if (idx > 0):
-            origin = (apt[:idx] + addr).replace(' ','+')
+            origin = (apt[:idx] + ', ' + addr)
         else:
-            origin = (apt + ' ' + addr).replace(' ','+')
+            origin = (apt + ', ' + addr)
+        # Remove 'Apartments' word if it's a street address, e.g., 3408 Parkview Avenue Apartments
+        if (origin[0].isdigit()):
+            origin = origin.replace(' Apartments', '');
+        # Replace special symbols for google map format
+        origin = origin.replace(' ', '+').replace('/', '%2F')
         link = 'https://www.google.com/maps/dir/?api=1&origin='+origin+'&destination='+destination+'&travelmode='+tm
+        iprint(link)
         html = urlopen(link)
         bsyc = BeautifulSoup(html.read(), "lxml")
         time.sleep(5)
@@ -104,8 +118,8 @@ iprint(map_data)
 idx = 0
 for apt,addr in zip(apt_list, addr_list):
     idx += 1;
-    # Skip apt that exists in DF
-    if apt == 'Address Not Disclosed' or apt in map_data.index:
+    # Skip invalid or searched apt
+    if ' Not ' in apt or 'bed' in apt or '$' in apt or apt in map_data.index:
         continue
     try:
         # Add row to DataFrame
@@ -118,7 +132,7 @@ for apt,addr in zip(apt_list, addr_list):
 
 # Check updated DF
 iprint('\nUpdating map_data done.')
-iprint(map_data)
+#iprint(map_data)
 # Save our DF
-map_data.to_csv(filename)
+map_data.to_csv(mapFile)
 
